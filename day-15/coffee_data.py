3c97ff423306ed21f61652ca533fe69a4cab2e66
Ryan Webster <ryanwebster94@protonmail.com>
2026-01-13 02:03:07 -0800

    Added coffee_machine and coffee_data. Added Data

---
+++ day-15/coffee_data.py
@@ -0,0 +1,29 @@
+MENU = {"latte":
+        {"ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        },
+        "espresso":
+        {"ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        },
+        "cappuccino":
+        {"ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+profit = 0
