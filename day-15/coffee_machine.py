1e82d65d2ef05a593cc2a7b4240b371d19fcbe56
Ryan Webster <ryanwebster94@protonmail.com>
2026-01-15 20:12:27 -0800

    Staging changes while offline

--- day-15/coffee_machine.py
+++ day-15/coffee_machine.py
@@ -5,21 +5,12 @@ def admin_panel():
     """
     This needs to be streamlined, please give it another try.
     """
-    admin = "admin"
-    password = "1234"
-
+    admin_username = "Ryan"
+    admin_password = "1234"
     attempts = 0
-    while attempts < 3:
-        username = input("Enter your user name:\n")
-        if username != admin:
-            print("Please try again.")
-        if username == admin:
-            enter_password = input("Enter your password:\n")
-            if enter_password != password:
-                attempts += 1
-                print(f"Password invalid: {3 - attempts} remaining")
-            
 
+    while attempts < 3:
+        
 
 def process_payment():
     pass
